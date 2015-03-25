4838914c2a939737edbd158accb9c91417be397b
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-25 10:40:35 +0300

    refactors

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -7,6 +7,6 @@ from tests import TESTS
 
 class Referee(RefereeBase):
     TESTS = TESTS
-    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
-    CURRENT_ENV = settings_env.CURRENT_ENV
-    FUNCTION_NAME = "roman"
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
+    DEFAULT_FUNCTION_NAME = "roman"
